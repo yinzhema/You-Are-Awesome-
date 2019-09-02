36018adc85dd73a181b0c733ac5283fa6d9f4a56
Yinzhe Ma <mabp@bc.edu>
2019-09-01 22:37:13 -0400

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by MYZ on 9/1/19.
+//  Copyright © 2019 Yinzhe Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ You Are Awesome!Tests/You_Are_Awesome_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  You_Are_Awesome_Tests.swift
+//  You Are Awesome!Tests
+//
+//  Created by MYZ on 9/1/19.
+//  Copyright © 2019 Yinzhe Ma. All rights reserved.
+//
+
+import XCTest
+@testable import You_Are_Awesome_
+
+class You_Are_Awesome_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
